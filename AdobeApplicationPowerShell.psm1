8d4536e87747eb01638752e45644fbac2c7a0abf
Chris Magnuson <hourglasssand@gmail.com>
2018-11-28 16:22:43 -0500

    Update Invoke-AdobeApplicationJSX

--- AdobeApplicationPowerShell.psm1
+++ AdobeApplicationPowerShell.psm1
@@ -7,10 +7,10 @@ $AdobeApplications = [PSCustomObject]@{
 
 function Invoke-AdobeApplicationJSX {
     param (
-        [ValidateSet("Illustrator","InDesign")][Parameter(Mandatory)]$AdobeApplicationName,
+        [ValidateSet("Illustrator","InDesign")][Parameter(Mandator,ParameterSetName="AdobeApplicationName")]$AdobeApplicationName,
+        [Parameter(Mandator,ParameterSetName="AdobeApplicationCOMObject")]$AdobeApplicationCOMObject,
         [Parameter(Mandatory,ValueFromPipeline,ParameterSetName="JSXFilePath")]$JSXFilePath,
-        [Parameter(Mandatory,ValueFromPipeline,ParameterSetName="JSXFileContent")]$JSXFileContent,
-        $AdobeApplicationCOMObject
+        [Parameter(Mandatory,ValueFromPipeline,ParameterSetName="JSXFileContent")]$JSXFileContent
     )
     begin {
         if (-not $AdobeApplicationCOMObject) {
@@ -23,6 +23,7 @@ function Invoke-AdobeApplicationJSX {
             $JSXFilePath = [IO.Path]::GetTempFileName()
             $JSXFileContent | Out-File -FilePath $JSXFilePath
         }
+        $AdobeApplicationCOMObject.DoJavaScriptFile($JSXFilePath)
     }
     end {
         if ($AdobeApplicationOpenedWithinFunction) {
