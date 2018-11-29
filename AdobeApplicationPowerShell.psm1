2933bc1c3c0874eff3dacef102f75bc07070c008
Chris Magnuson <hourglasssand@gmail.com>
2018-11-29 09:37:43 -0500

    Add first working version to support InDesign

--- AdobeApplicationPowerShell.psm1
+++ AdobeApplicationPowerShell.psm1
@@ -1,29 +1,34 @@
-$AdobeApplications = [PSCustomObject]@{
-    Name = "InDesign"
-},
-[PSCustomObject]@{
-    Name = "Illustrator"
-}
-
 function Invoke-AdobeApplicationJSX {
     param (
-        [ValidateSet("Illustrator","InDesign")][Parameter(Mandator,ParameterSetName="AdobeApplicationName")]$AdobeApplicationName,
-        [Parameter(Mandator,ParameterSetName="AdobeApplicationCOMObject")]$AdobeApplicationCOMObject,
-        [Parameter(Mandatory,ValueFromPipeline,ParameterSetName="JSXFilePath")]$JSXFilePath,
-        [Parameter(Mandatory,ValueFromPipeline,ParameterSetName="JSXFileContent")]$JSXFileContent
+        [ValidateSet("Illustrator","InDesign")][Parameter(Mandatory)]
+        $AdobeApplicationName,
+        
+        [Parameter(Mandatory,ValueFromPipeline,ParameterSetName="JSXFilePath")]
+        $JSXFilePath,
+        
+        [Parameter(Mandatory,ValueFromPipeline,ParameterSetName="JSXFileContent")]
+        $JSXFileContent,
+
+        $AdobeApplicationCOMObject
     )
     begin {
         if (-not $AdobeApplicationCOMObject) {
             $AdobeApplicationCOMObject = New-Object -ComObject "$AdobeApplicationName.Application"
             $AdobeApplicationOpenedWithinFunction = $True
+            Start-Sleep -Seconds 1
         }
     }
     process {
         if (-not $JSXFilePath) {
-            $JSXFilePath = [IO.Path]::GetTempFileName()
+            $JSXFilePath = [IO.Path]::GetTempFileName() -replace "\.tmp",".jsx" #InDesign errors if extension is not jsx
             $JSXFileContent | Out-File -FilePath $JSXFilePath
         }
-        $AdobeApplicationCOMObject.DoJavaScriptFile($JSXFilePath)
+
+        if ($AdobeApplicationName -eq "InDesign") {
+            $AdobeApplicationCOMObject.DoScript($JSXFilePath, 1246973031)
+        } elseif ($AdobeApplicationName -eq "Illustrator") {
+            $AdobeApplicationCOMObject.DoJavaScriptFile($JSXFilePath)
+        }
     }
     end {
         if ($AdobeApplicationOpenedWithinFunction) {
